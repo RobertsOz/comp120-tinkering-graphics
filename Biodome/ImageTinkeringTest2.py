bc932886c5c10c95590ef7587fdd45068bd437e8
KieranGolds <Kieranalexstrasza@gmail.com>
2016-10-28 12:23:45 +0100

    Alterations to Biodome code

--- Biodome/ImageTinkeringTest2.py
+++ Biodome/ImageTinkeringTest2.py
@@ -2,28 +2,36 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-clock = pygame.time.Clock()
-pygame.mouse.set_visible(0)
+pygame.mouse.set_visible(1)
 
 
-window_width = 1920
-window_height = 1080
+window_width = 800
+window_height = 600
 window_size = (window_width, window_height)
+R1 = 255
+G1 = 255
+B1 = 255
+R2 = 200
+G2 = 200
+B2 = 200
 
 screen = pygame.display.set_mode(window_size)
-bg = pygame.image.load("C:\Users\Kieran\Desktop\Tinkering\Biodome.jpg")
-screen.fill((0,0,0))
-screen.blit(bg (0,0))
+bg = pygame.image.load("Biodome.jpg").convert_alpha()
+
+# pixelarray
+for x in range(bg.get_width()):
+        for y in range (bg.get_height()):
+            if bg.get_at((x,y)) == ((R1,B1,G1)):
+                bg.set_at((x,y), (255,255,255,0))
 
 while True:
-
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
-clock.tick(60)
+    screen.fill((255,0,0))
+    screen.blit(bg, (0,0))
 
-pygame.display.flip()
-pygame.display.update()
+    pygame.display.update()
